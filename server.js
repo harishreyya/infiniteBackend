652ecd00dc36628ef453989bea27254f7a2f3b21
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-06-30 17:44:20 +0530

    for deoloyment

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const db_connect = "mongodb+srv://hari:hari@cluster0.hsmpfuf.mongodb.net/loading?retryWrites=true&w=majority"
+//const db_connect =  "mongodb://localhost:27017/loading"
+const connect = () => {
+    return mongoose.connect(db_connect,{
+        // useCreateIndex:true,
+        // useFindAndModify:false,
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    });
+  };
+
+  const app = express()
+  app.use(express.json())
+  app.use(cors())
+
+const contentSchema = new mongoose.Schema(
+    {
+      serial:{type: Number, required: true},
+      title: { type: String, required: true },
+      desc: { type: String, required: true },
+      video: { type: String, required: true }
+    }
+  );
+
+const Content = mongoose.model("content",contentSchema)
+
+app.get('/contents', async (req,res) =>{
+  const { page = 1, limit = 10 } = req.query;
+    const contents = await Content.find({}).skip((page - 1) * limit)
+    .limit(Number(limit)).lean().exec()
+    res.send(contents)
+})
+
+app.post('/contents', async (req,res) =>{
+    const contents = await Content.create(req.body)
+    res.status(201).json({contents})
+})
+
+app.listen(8000, async ()=>{
+    await connect()
+    console.log("listing on port 8000")
+})
